94397c89d7833b9847026d3c3bdc01857184b1a5
sunny <sunnyy@gmail.com>
2024-09-02 19:07:24 +1000

    validation

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -14,9 +14,18 @@ export class EmployeeComponent implements OnInit {
       Validators.required
     ]),
     lastName: new FormControl<string>('', [Validators.minLength(3), Validators.required]),
-    age: new FormControl<number|null>(null, [Validators.min(18), Validators.required])
+    age: new FormControl<number|null>(null, [ Validators.required]),
+    licenseNumber: new FormControl<string>('')
   });
   constructor() { }
   ngOnInit() {
+    this.bioSection.get('age')?.events.subscribe((event) => {
+      if (parseInt(event.source.value as string) > 18) {
+        this.bioSection.get('licenseNumber')?.addValidators(Validators.required);
+      } else {
+        this.bioSection.get('licenseNumber')?.removeValidators(Validators.required);
+      }
+      this.bioSection.get('licenseNumber')?.updateValueAndValidity();
+    });
   }
 }
